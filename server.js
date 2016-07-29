3625eb8c8fefd6bca2670171cf444e8dca48bb27
Dan O'Meara <omeara.dan@gmail.com>
2016-07-28 22:58:53 -0700

    first commit of new code

--- server.js
+++ server.js
@@ -1,34 +1,83 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var stormpath = require('express-stormpath');
-var routes = require("./app/routes");
-var db = require('./config/db');
-var security = require('./config/security');
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
-var morgan = require('morgan');
-app.use(morgan);
-app.use(stormpath.init(app, {
-  apiKeyFile: './config/stormpath_apikey.properties',
-  application: 'SP APPLICATION URL',
-  secretKey: security.stormpath_secret_key
-}));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
-var port = 8000;
-mongoose.connect(db.url);
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-routes.addAPIRouter(app, mongoose, stormpath);
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
 
-app.use(function(req, res, next){
-  res.status(404);
-  res.json({ error: 'Invalid URL' });
+  // Initialize the app.
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
 });
 
-app.listen(port);
+// CONTACTS API ROUTES BELOW
 
-console.log('Magic happens on port ' + port);
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
 
-exports = module.exports = app;
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/contacts", function(req, res) {
+});
+
+app.post("/contacts", function(req, res) {
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+});
+
+app.put("/contacts/:id", function(req, res) {
+});
+
+app.delete("/contacts/:id", function(req, res) {
+});
+
+app.post("/contacts", function(req, res) {
+  var newContact = req.body;
+  newContact.createDate = new Date();
+
+  if (!(req.body.firstName || req.body.lastName)) {
+    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  }
+
+  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new contact.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
